eaacf85bbbe3c94a40cbde397a1f9eef9bfae5a8
Vuong Gia Hy <hyvuonggia@gmail.com>
2022-07-16 20:51:27 +0700

    Product Of Array Except Self

---
+++ array_and_hashing/ProductOfArrayExceptSelf.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    var result = [];
+
+    let product = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = product;
+        product *= nums[i];
+    }
+
+    product = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= product;
+        product *= nums[i];
+    }
+
+    return result;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
